e69b85f84dda8ced445d69b390621c6c52e8c8a0
Angor78 <angor78@gmail.com>
2022-06-01 14:54:00 +0300

    new file component AddItemComponent

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,5 +1,6 @@
-import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
+import React, {ChangeEvent} from 'react';
 import {FilterValuesType} from './App';
+import {AddItemComponent} from "./AddItemComponent";
 
 export type TaskType = {
   id: string
@@ -62,48 +63,6 @@ export function Todolist(props: PropsType) {
       </button>
     </div>
   </div>
-}
-
-export type addItemComponentPropsType = {
-  addTask: (newTitle: string, id: string) => void
-  id: string
-}
-const AddItemComponent = (props: addItemComponentPropsType) => {
-  let [title, setTitle] = useState("")
-  let [error, setError] = useState<string | null>(null)
-
-  const addTask = () => {
-    let newTitle = title.trim();
-    if (newTitle !== "") {
-      props.addTask(newTitle, props.id);
-      setTitle("");
-    } else {
-      setError("Title is required");
-    }
-  }
-
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.currentTarget.value)
-  }
-
-  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-    setError(null);
-    if (e.charCode === 13) {
-      addTask();
-    }
-  }
-
-  return (
-    <div>
-      <input value={title}
-             onChange={onChangeHandler}
-             onKeyPress={onKeyPressHandler}
-             className={error ? "error" : ""}
-      />
-      <button onClick={addTask}>+</button>
-      {error && <div className="error-message">{error}</div>}
-    </div>
-  )
 }
 
 
